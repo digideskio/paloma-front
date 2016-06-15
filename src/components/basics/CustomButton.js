464b9272f1dd133f822df09ec41897954e06f378
Emilien <emilien_muraton@yahoo.fr>
2016-06-15 16:23:09 +0200

    Add button component.

---
+++ src/components/basics/CustomButton.js
@@ -0,0 +1,37 @@
+require('styles/bootstrap.css');
+require('styles/bootstrap-theme.css');
+require('styles/style.css');
+
+import {Button} from 'react-bootstrap';
+import React from 'react';
+import classNames from 'classnames';
+
+class CustomButton extends React.Component {
+
+    static propTypes = {
+        onChange: React.PropTypes.func.isRequired,
+        label: React.PropTypes.string.isRequired,
+        bsSize: React.PropTypes.string,
+        isDisabled: React.PropTypes.bool,
+        type: React.PropTypes.string
+
+    }
+
+    static defaultProps = {
+        bsSize: 'large',
+        type: 'button',
+        isDisabled: true
+    };
+
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+        return (
+            <Button bsSize={this.props.bsSize} type={this.props.type} disabled={this.props.isDisabled}>{this.props.label}</Button>
+        );
+    }
+}
+
+export default CustomButton;
