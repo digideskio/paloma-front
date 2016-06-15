4dc3e4c71e63cd95a8da6a792791cd3eaaf2ebbe
Emilien <emilien_muraton@yahoo.fr>
2016-06-15 16:22:16 +0200

    Add form date (no validator yet).

---
+++ src/components/basics/FormDate.js
@@ -0,0 +1,50 @@
+require('styles/bootstrap.css');
+require('styles/bootstrap-theme.css');
+require('styles/style.css');
+require('react-datetime/css/react-datetime.css');
+
+import DateTimeField from 'react-datetime';
+import React from 'react';
+import classNames from 'classnames';
+
+class FormDate extends React.Component {
+
+    static propTypes = {
+        onChange: React.PropTypes.func.isRequired,
+        value: React.PropTypes.instanceOf(Date),
+        viewMode: React.PropTypes.string,
+        dateFormat: React.PropTypes.string,
+        timeFormat: React.PropTypes.bool
+
+    }
+
+    static defaultProps = {
+        viewMode: 'days',
+        timeFormat: 'false',
+        dateFormat: 'DD/MM/YYYY'
+    };
+
+    constructor(props) {
+      super(props);
+    }
+
+    /*onChange(e) {
+        if(this.props.onChange){
+            this.props.onChange();
+        }
+    }*/
+
+    render() {
+        return (
+            <DateTimeField
+                dateFormat={this.props.dateFormat}
+                timeFormat={this.props.timeFormat}
+                onChange={this.props.onChange}
+                value={this.props.value}
+                viewMode={this.props.viewMode}
+            />
+        );
+    }
+}
+
+export default FormDate;
