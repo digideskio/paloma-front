824b5fee9e4f7dbdd68d36b81fc77a487803ea41
Emilien <emilien_muraton@yahoo.fr>
2016-06-15 11:47:30 +0200

    Add component form for input text (no validator yet).

---
+++ src/components/basics/FormText.js
@@ -0,0 +1,55 @@
+require('styles/bootstrap.css');
+require('styles/bootstrap-theme.css');
+require('styles/style.css');
+
+import {ControlLabel, FormControl, FormGroup} from 'react-bootstrap';
+import React from 'react';
+import classNames from 'classnames';
+
+class FormText extends React.Component {
+
+    static propTypes = {
+        placeHolderValue: React.PropTypes.string.isRequired,
+        labelText: React.PropTypes.string.isRequired,
+        id: React.PropTypes.string.isRequired,
+        value: React.PropTypes.string,
+        onChange: React.PropTypes.func,
+        onBlur: React.PropTypes.func,
+        validity: React.PropTypes.string
+    }
+
+    constructor(props) {
+      super(props);
+    }
+
+    onChange(e) {
+        if(this.props.onChange){
+            this.props.onChange();
+        }
+    }
+
+    onBlur(e) {
+        if(this.props.onChange){
+            this.props.onChange();
+        }
+    }
+
+    render() {
+        return (
+            <FormGroup controlId={'formText-' + this.props.id} validationState={this.props.validity}>
+                <ControlLabel>{this.props.labelText}</ControlLabel>
+                <FormControl
+                    componentClass='input'
+                    type='text'
+                    placeholder={this.props.placeHolderValue}
+                    onChange={this.onChange}
+                    onBlur={this.props.onBlur}
+                    value={this.props.text}
+                />
+                <FormControl.Feedback />
+            </FormGroup>
+        );
+    }
+}
+
+export default FormText;
